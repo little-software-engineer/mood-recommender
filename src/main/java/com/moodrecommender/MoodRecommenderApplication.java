9ac252216a59b08f9703f75da9e3dda270f27586
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2024-02-10 15:42:23 +0100

    Add files via upload

---
+++ src/main/java/com/moodrecommender/MoodRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.moodrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodRecommenderApplication.class, args);
+	}
+
+}
