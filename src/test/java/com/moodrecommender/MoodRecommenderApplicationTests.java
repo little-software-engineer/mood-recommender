3e94ed7a194a99f74b2c0c9fecd8ebc81c9c8414
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2024-02-10 15:46:46 +0100

    Add files via upload

--- src/test/java/com/moodrecommender/MoodRecommenderApplicationTests.java
+++ src/test/java/com/moodrecommender/MoodRecommenderApplicationTests.java
@@ -1,1 +1,13 @@
+package com.moodrecommender;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
